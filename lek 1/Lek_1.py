0bb3f1e68264c3ec73690049d8ead10b785ef3ba
DAPetrovich <93150473+DAPetrovich@users.noreply.github.com>
2021-11-06 11:38:52 +0200

    Update Lek_1.py

--- lek 1/Lek_1.py
+++ lek 1/Lek_1.py
@@ -1,34 +1,29 @@
-from typing import Match
-
-graf = ([0,2],[2,5],[5,2],[6,6],[8,3])
+graf = ((0,2),(2,5),(5,2),(6,6),(8,3))
 graf_len = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
 
-x = 0
-while x <= 4:
-    y = 0
-    while y <=4 :
-        xx = (graf[y][0] - graf[x][0]) ** 2
-        yy = (graf[y][1] - graf[x][1]) ** 2
-        temp = ( xx + yy ) ** 0.5
-        graf_len[x][y] = temp
-        y = y + 1
-    x = x + 1
-otvet = '(0,2) '
+def length_AtoB(x, y):
+    km = ((graf[y][0] - graf[x][0]) ** 2 + (graf[y][1] - graf[x][1]) ** 2)  ** 0.5
+    return(km)
+
+for x in range(5):
+    for y in range(5) :
+        graf_len[x][y] = length_AtoB(x, y)
+
+otvet = str(graf[0])
 length = 0
-i=0
 stroka = 0
-while i <4:
-    j=0 
+for i in range(4):
     my_min=10000
-    while j<=4:
-        if graf_len[stroka][j] < my_min and stroka != j:
+    for j in range(5):
+        if (graf_len[stroka][j] < my_min) and (stroka != j):
             my_min = graf_len[stroka][j] # сохраняем длину минимального пути
             ii=stroka # сохраняем индексы
-            jj=j # сохраняем индексы
-        j = j + 1
+            jj=j      # сохраняем индексы
     length = length + my_min
-    otvet = otvet + '-> ('+ str(graf[jj][0]) + ','+ str(graf[jj][1]) + ')' + '[' + str(length) + ']'
-    graf_len[jj][ii] = 10000
+    otvet = otvet + ' -> ' + str(graf[jj]) + '[' + str(length) + ']'
+    for z in range(5): graf_len[z][stroka] = 10000 # помечаем вершину чтобы не зайти в неё второй раз
     stroka = jj # переходим на соседнюю вершину с минималным расстоянием
-    i = i + 1
+
+length = length + length_AtoB(jj,0) 
+otvet = otvet + '-> ' + str(graf[0]) + '[' + str(length) + ']'
 print(otvet + ' = ' + str(length))
