0d4216ddc4013326e22ca03589e03ff4b870f9cb
DAPetrovich <93150473+DAPetrovich@users.noreply.github.com>
2022-01-31 13:28:35 +0200

    31,01,22

--- lek 1/Lek_1.py
+++ lek 1/Lek_1.py
@@ -1,19 +1,19 @@
 graf=[(0, 2), (2, 5), (5, 2), (6, 6), (8, 3) ]
-pyt =  [0,0,0,0,0]
+path =  [0,0,0,0,0]
 
 def length_AtoB(x, y):
     return ((graf[y][0] - graf[x][0]) ** 2 + (graf[y][1] - graf[x][1]) ** 2)  ** 0.5
 
-def vivod(pyt):
+def vivod(path):
     string = str(graf[0])  
     i=1
     length = 0
     while i < 5:
-        length += length_AtoB(pyt[i-1], pyt[i])
-        string += ' -> ' + str(graf[pyt[i]]) + '[' +str(length) + ']'
+        length += length_AtoB(path[i-1], path[i])
+        string += ' -> ' + str(graf[path[i]]) + '[' +str(length) + ']'
         i += 1
-    length += length_AtoB(pyt[i-1], pyt[0]) 
-    string += ' ->' + str(graf[pyt[0]]) + '[' + str(length) + ']' + ' = ' + str(length)
+    length += length_AtoB(path[i-1], path[0]) 
+    string += ' ->' + str(graf[path[0]]) + '[' + str(length) + ']' + ' = ' + str(length)
     return string
 
 def calculate():
@@ -26,10 +26,7 @@ def calculate():
                         min_pyt_temp = length_AtoB(0,i) + length_AtoB(i,j) + length_AtoB(j,x) + length_AtoB(x,y) + length_AtoB(y,0)
                         if min_pyt  > min_pyt_temp: # если новый путь короче старого то пересохраняемся
                             min_pyt = min_pyt_temp                
-                            pyt[1] = i
-                            pyt[2] = j
-                            pyt[3] = x
-                            pyt[4] = y
-    return pyt
+                            path[1], path[2], path[3], path[4] = i, j, x ,y
+    return path
 
 print(vivod(calculate()))
